21b3da0030a152f6a285b622ffeab91439691ad9
guilhermelima <guilhermelimaruby@gmail.com>
2024-04-29 10:35:02 -0300

    projeto base

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,3 @@
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
-
 exports.showIndex = (req, res, next) => {
     res.render('index')
 }
@@ -15,50 +12,4 @@ exports.showMembersPage = (req, res) => {
 
 exports.get404Page = (req, res, next) => {
     res.status(404).render('404')
-}
-
-exports.signup = async(req, res, next) => {
-    const {username, email, password} = req.body
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = new User(username, email, hashedPassword)
-    try {
-        user.save()
-        res.redirect('/')
-    } catch (err) {
-        console.log(err);
-        res.redirect('signup')
-    }
-}
-
-exports.login = async(req, res, next) => {
-    const {email, password} = req.body
-    const user = await User.findOne(email, password)
-    try {
-        if (user) {
-            req.session.user = user
-            res.redirect('/members')
-        } else {
-            res.render('index')
-        }
-    } catch (err) {
-        console.log(err)
-        res.render('index')
-    }
-}
-
-exports.checkAuth = (req, res, next) => {
-    if (req.session && req.session.user || req.isAuthenticated()) {
-        next()
-    } else {
-        res.redirect('/')
-    }
-}
-
-exports.logout = (req, res, next) => {
-    req.session.destroy(err => {
-        if (err) {
-            console.log(err);
-        }
-        res.redirect('/')
-    })
 }
